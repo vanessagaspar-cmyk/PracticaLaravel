f1c56d7006c70faf036cb8cfe48cd368560dbd89
vanessagaspar-cmyk <vanessa.gaspar@gmail.com>
2026-01-05 21:32:04 +0000

    Prueba POST1

--- src/app/Http/Controllers/EventoController.php
+++ src/app/Http/Controllers/EventoController.php
@@ -33,48 +33,7 @@ class EventoController extends Controller
      */
     public function store(Request $request)
     {
-        //Validar que la petición contenga todos los datos necesarios
-        $validator = Validator::make($request->all(), [
-            'titulo'=> 'required',
-            'descripcion' => 'required',
-            'fecha_inicio' => 'required|date',
-            'fecha_fin' => 'required|date',
-            'ubicacion' => 'required',
-        ]);
-
-        //Si la petición no contiene todos los datos necesarios, retornar un  mensaje de error
-        if ($validator->fails()) {
-            $respuesta = [
-                'message' => 'Datos faltantes',
-                'status' => 400, //Petición inválida
-            ];            
-            return response()->json($respuesta, 400);
-        }  
-        
-        //Crear un nuevo recursos con los datos de la petición
-        $evento = Evento::create([
-            'titulo'=> $request->titulo,
-            'descripcion' => $request->descripcion,
-            'fecha_inicio' => $request->fecha_inicio,
-            'fecha_fin' => $request->fecha_fin,
-            'ubicacion' => $request->ubicacion,
-        ]);
-
-        //Si el recurso no se pudo crear, retornar un mensaje de error
-        if (!$evento){
-            $respuesta = [
-                'message' => 'Error al crear el evento',
-                'status' => 500, //Error interno del servidor
-            ];
-            return response()->json($respuesta, 500);
-        }
-
-        //Retornar el recurso creado
-        $respuesta = [
-            'evento' => $evento,
-            'status' => 201,
-        ];
-        return response()->json($respuesta, 201);
+      return response()->json(['debug' => 'Llegué al controlador'], 200);
     }
 
     /**
