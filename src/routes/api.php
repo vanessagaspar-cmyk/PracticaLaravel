3ec3bd2e166270e629799853aa03425792a8f8ad
vanessagaspar-cmyk <vanessa.gaspar@gmail.com>
2026-01-05 17:15:36 +0000

    Gaurdado Etapa 2

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+//Agregar el controlador EventoController
+use App\Http\Controllers\EventoController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:api');
+
+/**
+ * Rutas para el recurso Evento
+ */
+
+//Recuperar todos los eventos
+Route::get('/eventos',[EventoController::class,'index']);    
+
+//Almacenar un evento nuevo
+Route::post('/eventos',[EventoController::class,'store']);
+
+//Recuperar un evento específico
+Route::get('/eventos/{id}',[EventoController::class,'show']);
+
+//Actualizar un evento específico
+Route::put('/eventos/{evento}',[EventoController::class,'update']);
+
+//Eliminar un evento específico
+Route::delete('/eventos/{id}',[EventoController::class,'destroy']);
